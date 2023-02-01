8dfb70f74a525ebffff687bd65b73ed13693b34f
Дмитрий Ерыганов <dnevvs@yandex.ru>
2023-02-01 13:20:55 +0300

    Final 8 2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, PostViewSet, api_group, api_group_detail
+from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register('api/v1/posts', PostViewSet, basename='posts')
+router.register('api/v1/groups', GroupViewSet, basename='groups')
 router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
@@ -15,7 +16,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api/v1/api-token-auth/', views.obtain_auth_token,
          name='api_token'),
-    path('api/v1/groups/', api_group),
-    path('api/v1/groups/<int:pk>/', api_group_detail)
 
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,8 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from posts.models import Comment, Group, Post
-from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.exceptions import MethodNotAllowed
 
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
@@ -51,24 +50,9 @@ class CommentViewSet(viewsets.ModelViewSet):
         return new_queryset
 
 
-@api_view(['GET'])
-def api_group(request):
-    group = Group.objects.all()
-    serializer = GroupSerializer(group, many=True)
-    return Response(serializer.data)
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
-
-@api_view(['GET', 'DELETE', 'PUT', 'PATCH'])
-def api_group_detail(request, pk):
-    group = Group.objects.get(id=pk)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        serializer = GroupSerializer(group, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        Group.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    serializer = GroupSerializer(group)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    def perform_create(self, serializer):
+        raise MethodNotAllowed('Добавление группы запрещено!')
